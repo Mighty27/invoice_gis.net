f482fae0fca9ba799c52ae1c3c3c8037834d38db
mighty27 <81411396+Mighty27@users.noreply.github.com>
2024-05-30 15:36:24 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ const captureButton = document.getElementById("capture-button");
 var captureButtonStyle = document.getElementById("capture-button");
 const previewCapture = document.getElementById("preview-capture");
 const downloadButton = document.getElementById("download-button");
+const printButton = document.getElementById("print-button");
 const closeButton = document.getElementById("close-button");
 var element = document.getElementById("cover");
 const gisButton = document.getElementById("gis");
@@ -15,16 +16,39 @@ var tagLine = document.getElementById("tag-line");
 
 captureButton.addEventListener("click", async () => {
     captureButtonStyle.classList.add("invisible");
-    element.style.justifyContent = "unset";
-    const canvas = await html2canvas(mediaCapture);
-    const imgURL = canvas.toDataURL();
-    previewCapture.innerHTML = `<img src="${imgURL}" id="image" style="width: 500px; height:auto;">`;
-    downloadButton.href = imgURL;
-    downloadButton.download = "image.png";
+    mediaCapture.style.border = "unset";
+    var element = jQuery("#mediaCanvas")[0];
+    html2canvas($(element)[0], {
+        scale:2
+        }).then(function(canvas) {
+        const imgURL = canvas.toDataURL();
+        previewCapture.innerHTML = "<img id='Image' src=" + imgURL + " style='width:1000px!important;'></img>";
+        downloadButton.href = imgURL;
+        downloadButton.download = "image.png"
+        });
+
+    printButton.addEventListener("click", ()=> {
+        var element = jQuery("#mediaCanvas")[0];
+        html2canvas($(element)[0], {
+            scale:3
+          }).then(function(canvas) {
+            var myImage = canvas.toDataURL("image/png");
+            var tWindow = window.open("");
+            $(tWindow.document.body).html("<img id='Image' src=" + myImage + " style='width:720px;'></img>").ready(function() {
+              tWindow.focus();
+              tWindow.print();
+            });
+          });
+        
+    });
+    
+
+
 });
 
+
 closeButton.addEventListener("click", async () => {
-    element.style.justifyContent = "center";
+    mediaCapture.style.border = "1px #D3D3D3 solid";
     captureButtonStyle.classList.remove("invisible");
 });
 
